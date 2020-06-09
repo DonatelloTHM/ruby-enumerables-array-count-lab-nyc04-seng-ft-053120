9a100c397a9d229ff6f935b1b9e7ae796184c55f
Donat Pllana <donat.pllana@outlook.com>
2020-06-09 15:44:28 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,7 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   array.count{|i|
-    if(i.class==String)
+    i.class==String
   }
 end
 
